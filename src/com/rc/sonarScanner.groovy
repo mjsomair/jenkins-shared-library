dccf3f77918f6b924b1b124a8e894f9951ba11c7
mjsomair <97043931+mjsomair@users.noreply.github.com>
2022-04-19 23:48:58 +0530

    Update sonarScanner.groovy

--- src/com/rc/sonarScanner.groovy
+++ src/com/rc/sonarScanner.groovy
@@ -81,4 +81,15 @@ class SonarScanner {
             }
         }
     }
+    
+    def runSonarScanner()
+    {
+        def config = config()
+        def SQscannerHome = tool config.sonarqube_installation_tool
+        withSonarQubeEnv(config.sonarqube_installation_name) 
+        {
+           bat "${SQscannerHome}/bin/sonar-scanner -Dsonar.projectKey=develop -Dsonar.sources=src"
+        }
+    }
+    
 }
