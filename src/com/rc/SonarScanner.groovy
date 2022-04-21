660e04bf9981e9f5f5301033dc73b394d335a0b2
mjsomair <97043931+mjsomair@users.noreply.github.com>
2022-04-21 23:36:55 +0530

    Update SonarScanner.groovy

--- src/com/rc/SonarScanner.groovy
+++ src/com/rc/SonarScanner.groovy
@@ -2,7 +2,6 @@ class SonarScanner
 {
     public static runSonarScanner()
         {     
-          mvn clean install  
-          mvn sonar:sonar -Dsonar.projectKey=develop -Dsonar.sources=src
+          bat "mvn clean sonar:sonar"  
         }
   }
