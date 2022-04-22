22bee97ea64aad193c5149c81ff4747030cc1df4
mjsomair <97043931+mjsomair@users.noreply.github.com>
2022-04-22 19:54:56 +0530

    Update sonarscanner.groovy

--- vars/sonarscanner.groovy
+++ vars/sonarscanner.groovy
@@ -6,7 +6,7 @@ def call(String stageName)
        def SQscannerHome = tool config.sonarqube_installation_tool
        withSonarQubeEnv(config.sonarqube_installation_name) 
         {
-          bat "${SQscannerHome}/bin/sonar-scanner -Dsonar.projectKey=develop -Dsonar.sources='C:\Users\mohammed jafar sadiq\.jenkins\workspace\jenkins-shared-library\src'"  
+          bat "${SQscannerHome}/bin/sonar-scanner -Dsonar.projectKey=develop -Dsonar.sources=C:\Users\mohammed jafar sadiq\.jenkins\workspace\jenkins-shared-library\src"  
         }
      }
    }  
